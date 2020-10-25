49c5954dfce1e0e81a5d5845f04f3f940086c55c
Anatoly Gladky <anatoliy.gladkiy@gmail.com>
2020-10-26 00:32:20 +0300

    Updated python-telegram-bot

--- main.py
+++ main.py
@@ -1,71 +1,100 @@
 """ Bot to emulate ball 8. """
 
 import logging
-import config
 import gettext
-
+import random
 from uuid import uuid4
-from telegram.ext import (Updater, CommandHandler,
-                          InlineQueryHandler, RegexHandler)
-from telegram import (InlineQueryResultArticle, InputTextMessageContent,
-                      ReplyKeyboardMarkup)
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    InlineQueryHandler,
+    MessageHandler,
+    Filters,
+)
+from telegram import (
+    InlineQueryResultArticle,
+    InputTextMessageContent,
+    ReplyKeyboardMarkup,
+)
+import config
 
-SUPPORTED_LANGUAGES = ['en', 'ru']
-languages = [config.language] if config.language in SUPPORTED_LANGUAGES else [SUPPORTED_LANGUAGES[0]]
-translation = gettext.translation('base', localedir='locale', languages=languages)
-translation.install()
 
-# App imports
-from ball import Ball  # noqa: E402
-
+SUPPORTED_LANGUAGES = ["en", "ru"]
+languages = (
+    [config.language]
+    if config.language in SUPPORTED_LANGUAGES
+    else [SUPPORTED_LANGUAGES[0]]
+)
+translation = gettext.translation("base", localedir="locale", languages=languages)
+_ = translation.gettext
 
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO)
-
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
 logger = logging.getLogger(__name__)
-ball = Ball()
+
+answers = (
+    _("It is certain"),
+    _("It is decidedly so"),
+    _("Without a doubt"),
+    _("Yes - definitely"),
+    _("You may rely on it"),
+    _("As I see it, yes"),
+    _("Most likely"),
+    _("Outlook good"),
+    _("Yes"),
+    _("Signs point to yes"),
+    _("Reply hazy, try again"),
+    _("Ask again later"),
+    _("Better not tell you now"),
+    _("Cannot predict now"),
+    _("Concentrate and ask again"),
+    _("Don't count on it"),
+    _("My reply is no"),
+    _("My sources say no"),
+    _("Outlook not so good"),
+    _("Very doubtful"),
+)
 
 
-def start(bot, update):
+def start(update, context):
     """
     Send a message when the command /start is issued.
     """
-    reply_markup = ReplyKeyboardMarkup([[_('Shake the 🎱')]],
-                                       resize_keyboard=True)
-    update.message.reply_text('{}:'.format(_('Choose')), reply_markup=reply_markup)
+    reply_markup = ReplyKeyboardMarkup([[_("Shake the 🎱")]], resize_keyboard=True)
+    update.message.reply_text("{}:".format(_("Choose")), reply_markup=reply_markup)
 
 
-def help(bot, update):
+def help(update, context):
     """
     Send a message when the command /help is issued.
     """
-    update.message.reply_text(_('Help'))
+    update.message.reply_text(_("Help"))
 
 
-def error(bot, update, error):
+def error(update, context):
     """
     Log Errors caused by Updates.
     """
-    logger.warning('Update "%s" caused error "%s"', update, error)
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 
-def shake(bot, update):
+def shake(update, context):
     """
     Emulation shaking of 8 ball. Send random answer.
     """
-    update.message.reply_text(ball.shake())
+    update.message.reply_text(random.choice(answers))
 
 
-def inline_shake(bot, update):
+def inline_shake(update, context):
     """
     Emulation shaking of 8 ball. Send random answer inline.
     """
     results = [
         InlineQueryResultArticle(
             id=uuid4(),
-            title=_('Shake the 🎱'),
-            input_message_content=InputTextMessageContent(ball.shake())
+            title=_("Shake the 🎱"),
+            input_message_content=InputTextMessageContent(random.choice(answers)),
         ),
     ]
     update.inline_query.answer(results, cache_time=0)
@@ -78,15 +107,14 @@ def main():
     updater = Updater(token=config.bot_token)
     dispatcher = updater.dispatcher
 
-    dispatcher.add_handler(CommandHandler('start', start))
-    dispatcher.add_handler(CommandHandler('help', help))
-    dispatcher.add_handler(CommandHandler('shake', shake))
-    dispatcher.add_handler(RegexHandler('^{}$'.format(_('Shake the 🎱')), shake))
-
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help))
+    dispatcher.add_handler(CommandHandler("shake", shake))
+    dispatcher.add_handler(
+        MessageHandler(Filters.regex("^{}$".format(_("Shake the 🎱"))), shake)
+    )
+    dispatcher.add_handler(InlineQueryHandler(inline_shake))
     dispatcher.add_error_handler(error)
-
-    inline_caps_handler = InlineQueryHandler(inline_shake)
-    dispatcher.add_handler(inline_caps_handler)
 
     updater.start_polling()
     updater.idle()
